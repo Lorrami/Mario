b93fd9895c634168c10c0bd824b00a231874313b
BloodRedTape <gefestrale@gmail.com>
2021-12-26 01:03:04 +0200

    spawn method

--- NotHehe/GameObject.cs
+++ NotHehe/GameObject.cs
@@ -9,4 +9,11 @@ abstract class GameObject{
     public abstract void Update(float dt);
 
     public abstract void Render(RenderWindow window);
+
+    public void Spawn(GameObject obj){
+        if(OwningLevel is null)
+            throw new Exception("Null owning level");
+        
+        OwningLevel.SpawnObject(obj);
+    }
 }
