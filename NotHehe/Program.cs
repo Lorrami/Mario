5e8137c3c0a12d9c1c373e346653de6b9b0deb5a
Lorrami <r.riabinin.ik12@kpi.ua>
2021-12-25 19:03:22 +0200

    First try

---
+++ NotHehe/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+
+class Program
+{
+    static void Main()
+    {
+        Huita();
+    }
+
+    private static void Huita()
+    {
+        _gameInizialization();
+    }
+
+    private static void _gameInizialization()
+    {
+        RenderWindow _window = new RenderWindow(new VideoMode(100, 100), "Mario");
+        while (_window.IsOpen)
+        {
+            
+        }
+    }
+}
