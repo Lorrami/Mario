813c9a65e9c410011132d8f31144f85e2a674bf4
E1Hephaestus <gefestroyale@gmail.com>
2022-01-25 01:00:46 +0200

    Normalize direction for uniform bullet movement

--- NotHehe/FirstLevel/Bullet.cs
+++ NotHehe/FirstLevel/Bullet.cs
@@ -6,7 +6,7 @@ class Bullet: GameObject{
     private readonly float _speed = 300;
     private float _lifetime = 0;
     public Bullet(Vector2f direction){
-        _direction = direction;
+        _direction = direction / (float)Math.Sqrt(direction.X * direction.X + direction.Y * direction.Y);
         Size = new Vector2f(10, 10);
         Origin = Size/2;
         FillColor = Color.Red;
