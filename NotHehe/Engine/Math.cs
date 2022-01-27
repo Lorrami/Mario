e892388b7fd2448a4b6adc08746910a34358194d
E1Hephaestus <gefestroyale@gmail.com>
2022-01-27 04:56:05 +0200

    Vector2f length extension

---
+++ NotHehe/Engine/Math.cs
@@ -0,0 +1,10 @@
+﻿using SFML.System;
+
+
+static class Vector2fMath 
+{
+    public static float Length(this Vector2f vec)
+    {
+        return MathF.Sqrt(vec.X * vec.X + vec.Y * vec.Y);
+    } 
+}
